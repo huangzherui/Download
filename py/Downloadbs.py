45eb89b2eb16ef5319108359c8de7255319c00c0
huangzherui <166521162+huangzherui@users.noreply.github.com>
2024-08-02 20:51:43 +0800

    Update Downloadbs.py

--- py/Downloadbs.py
+++ py/Downloadbs.py
@@ -8,8 +8,8 @@ def urldownload(url,filename):
         file.close()
     with open(filename,'wb') as file:
         file.write(down_res.content)
-urldownload(('http://github.com/huangzherui/Download/raw/main/zip/'+filename+'.7z.001'),('./'+filename+'/'+filename+'.7z.001'))
-urldownload(('http://github.com/huangzherui/Download/raw/main/zip/'+filename+'.7z.002'),('./'+filename+'/'+filename+'.7z.002'))
-urldownload('http://github.com/huangzherui/Download/raw/main/zip/7z.dll',('./'+filename+'/'+'7z.dll'))
-urldownload('http://github.com/huangzherui/Download/raw/main/zip/7z.exe',('./'+filename+'/'+'7z.exe'))
+urldownload(('http://github.com/huangzherui/Download/raw/zbhedit/zip/'+filename+'.7z.001'),('./'+filename+'/'+filename+'.7z.001'))
+urldownload(('http://github.com/huangzherui/Download/raw/zbhedit/zip/'+filename+'.7z.002'),('./'+filename+'/'+filename+'.7z.002'))
+urldownload('http://github.com/huangzherui/Download/raw/zbhedit/zip/7z.dll',('./'+filename+'/'+'7z.dll'))
+urldownload('http://github.com/huangzherui/Download/raw/zbhedit/zip/7z.exe',('./'+filename+'/'+'7z.exe'))
 os.system(('cd '+filename+'&&copy /b '+filename+'.7z.00* '+filename+'.7z&&7z x '+filename+'.7z'))
