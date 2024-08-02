e87143d6b25af20d69b4a5685b4e4a1a7794b568
huangzherui <166521162+huangzherui@users.noreply.github.com>
2024-08-02 22:01:20 +0800

    Update Downloadbs.py

--- py/Downloadbs.py
+++ py/Downloadbs.py
@@ -2,7 +2,7 @@ import requests,warnings,sys,os
 with open('marketmain.cfg','r',encoding='utf8') as file:
     programlistraw = file.read()
     programlist = eval(programlistraw)
-filename = programlist[sys.argv[1]]["version"]
+filename = str(programlist[int(sys.argv[1])]["version"])
 os.makedirs(filename)            
 
 warnings.filterwarnings('ignore')
@@ -15,9 +15,9 @@ def urldownload(url,filename):
         file.write(down_res.content)
 urldownload('http://github.com/huangzherui/Download/raw/zbhedit/zip/7z.dll',('./'+filename+'/'+'7z.dll'))
 urldownload('http://github.com/huangzherui/Download/raw/zbhedit/zip/7z.exe',('./'+filename+'/'+'7z.exe'))
-for i in range(programlist[sys.argv[1]]["spilttotal"]):
+for i in range(programlist[int(sys.argv[1])]["spilttotal"]):
     s = '%03d' % i+1
-    down_url = programlist[sys.argv[1]]["downloadurl"]
+    down_url = programlist[int(sys.argv[1])]["downloadurl"]
     down_url.replace('$Appname',filename)
     down_url.replace('$spiltnum',s)
     urldownload(down_url,('./'+filename+'/'+filename+'.7z.'+s))
