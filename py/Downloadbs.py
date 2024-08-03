5b6ccc3a530d86f35200c964563d9c4aeb868101
huangzherui <166521162+huangzherui@users.noreply.github.com>
2024-08-03 10:50:59 +0800

    Update Downloadbs.py

--- py/Downloadbs.py
+++ py/Downloadbs.py
@@ -2,8 +2,9 @@ import requests,warnings,sys,os
 with open('marketmain.cfg','r',encoding='utf8') as file:
     programlistraw = file.read()
     programlist = eval(programlistraw)
-filename = str(programlist[int(sys.argv[1])]["version"])
-os.makedirs(filename)            
+filename = str(programlist[int(sys.argv[1])]["name"])
+if not os.path.exists(filename):
+    os.makedirs(filename)            
 
 warnings.filterwarnings('ignore')
 def urldownload(url,filename):
@@ -13,12 +14,12 @@ def urldownload(url,filename):
         file.close()
     with open(filename,'wb') as file:
         file.write(down_res.content)
-urldownload('http://github.com/huangzherui/Download/raw/zbhedit/zip/7z.dll',('./'+filename+'/'+'7z.dll'))
-urldownload('http://github.com/huangzherui/Download/raw/zbhedit/zip/7z.exe',('./'+filename+'/'+'7z.exe'))
+#urldownload('http://github.com/huangzherui/Download/raw/zbhedit/zip/7z.dll',('./'+filename+'/'+'7z.dll'))
+#urldownload('http://github.com/huangzherui/Download/raw/zbhedit/zip/7z.exe',('./'+filename+'/'+'7z.exe'))
 for i in range(programlist[int(sys.argv[1])]["spilttotal"]):
-    s = '%03d' % i+1
+    s = '%03d' % int(i+1)
     down_url = programlist[int(sys.argv[1])]["downloadurl"]
-    down_url.replace('$Appname',filename)
-    down_url.replace('$spiltnum',s)
+    down_url = down_url.replace('$Appname',filename)
+    down_url = down_url.replace('$spiltnum',s)
     urldownload(down_url,('./'+filename+'/'+filename+'.7z.'+s))
 os.system(('cd '+filename+'&&copy /b '+filename+'.7z.00* '+filename+'.7z&&7z x '+filename+'.7z'))
