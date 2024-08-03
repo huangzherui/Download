007fa37d0136c944edcbfda3f214b38f737bace2
huangzherui <166521162+huangzherui@users.noreply.github.com>
2024-08-03 13:27:44 +0800

    Update Downloadfile.py

--- py/Downloadfile.py
+++ py/Downloadfile.py
@@ -1,9 +1,1 @@
-import requests,warnings,sys,os                   
-filename = sys.argv[1]
-warnings.filterwarnings('ignore')
-down_res = requests.get(url=url,verify=False)
-if not os.path.exists(filename):
-    file = open(filename,'a')
-    file.close()
-with open(filename,'wb') as file:
-    file.write(down_res.content)
+
