7bd990e408eba6ce8fd7280a817ebda1af2b3623
huangzherui <166521162+huangzherui@users.noreply.github.com>
2024-08-04 18:27:31 +0800

    Download

--- py/Download.py
+++ py/Download.py
@@ -1,14 +1,18 @@
 import os,wx,warnings,requests
 from tkinter import messagebox
 warnings.filterwarnings('ignore')
-isupdate = False
-def webdbget(user,password,tag):
-    import requests
+
+versions = 1.0#版本1.0
+readytoupdatelist = [] 
+readytoopenlist = []
+
+def webdbget(user,password,tag):#获取数据库
     postdata = {'user':user,'secret':password,'action':'get','tag':tag}
     r = requests.post('http://tinywebdb.appinventor.space/api',data=postdata)
     temp = r.text
     temp1 = eval(temp)[tag]
     return temp1
+
 def urldownload(url,filename):#下载文件
     down_res = requests.get(url=url,verify=False)
     if not os.path.exists(filename):
@@ -17,10 +21,10 @@ def urldownload(url,filename):#下载文件
     with open(filename,'wb') as file:
         file.write(down_res.content)
 
-def Download(num):#下载
+def Download(mode,AID):#下载
     #os.makedirs(filename)
     messagebox.showinfo('提示','下载中……')
-    os.system(('start python Downloadbs.py '+str(num)))
+    os.system(('start python Downloadbs.py '+mode+' '+str(AID)))
 
 def update():#更新
     urldownload('http://github.com/huangzherui/Download/raw/zbhedit/py/DownloadDownload.py','./DownloadDownload.py')
@@ -37,47 +41,52 @@ class MainWindow(wx.Frame):
         self.SetSize(400, 600)
         panel = wx.Panel(self)
         buttonlist = []
+        self.posnum = -1
         for i in range(len(programlist)):
-            if i == 0:
-                if isupdate:
-                    buttonlist.append(wx.Button(panel, label='有新版本，点我更新', pos=(0,0)))
-                    buttonlist[i].Bind(wx.EVT_BUTTON,lambda e,mark='update':self.OnButton(e, mark))
-                else:
-                    buttonlist.append(wx.Button(panel, label=programlist[i+1]["name"], pos=(0,0)))
-                    buttonlist[i].Bind(wx.EVT_BUTTON,lambda e,mark=i+1:self.OnButton(e, mark))
+            if programlist[i]['Programdeamon']:#如果是
+                if programlist[0]["version"] > versions:
+                    buttonlist.append(wx.Button(panel, label='有新版本，点我更新', pos=self.Calculate_the_location()))
+                    buttonlist[-1].Bind(wx.EVT_BUTTON,lambda e,mode='update',mark='0000':self.OnButton(e,mode, mark))
             else:
-                try:
-                    buttonlist.append(wx.Button(panel, label=programlist[i+(0 if isupdate else 1)]["name"], pos=(0,0+i*30)))
-                except:
-                    break
-                if i < 19:#第一列
-                    buttonlist[i].Bind(wx.EVT_BUTTON,lambda e,mark=i+(0 if isupdate else 1):self.OnButton(e, mark))
-                else:#第二列
-                    buttonlist[i].Bind(wx.EVT_BUTTON,lambda e,mark=i:self.OnButton(e, mark))
+                if not programlist[i]['InternalHide']:
+                    label=(programlist[i]["name"]+('(可更新)'if i in readytoupdatelist else '打开' if i in readytoopenlist else ''))
+                    buttonlist.append(wx.Button(panel, label=label, pos=self.Calculate_the_location()))
+                    buttonlist[-1].Bind(wx.EVT_BUTTON,lambda e,mode=('update'if i in readytoupdatelist else 'open' if i in readytoopenlist else 'download'),mark=i:self.OnButton(e,mode,mark))
         self.Centre()
-    def OnButton(self,e,name):
+    def OnButton(self,e,mode,AID):#按下按钮后
         self.Destroy()
-        if name == 'update':
+        if AID == '0000':
             update()
         else:
-            Download(name)
+            Download(mode,AID)
+    def Calculate_the_location(self):#计算位置
+        self.posnum+=1
+        return ((self.posnum//20)*100,self.posnum%20*30)
         
-if not os.path.isfile('Downloadbs.py'):
+if not os.path.isfile('Downloadbs.py'):#是否有Downloadbs.py
     urldownload('http://github.com/huangzherui/Download/raw/zbhedit/py/Downloadbs.py','./Downloadbs.py')
 
-versions = 1.0#版本1.0
-
 #打开marketmain.cfg
-#urldownload('https://github.com/huangzherui/Download/raw/zbhedit/marketmain.cfg','./marketmain.cfg')
 with open('marketmain.cfg','w',encoding='utf8') as file:
     programlist = webdbget('zhmarket','9d6ef697','marketapps')
     file.write(str(programlist))
 
-
-#检测版本
-if programlist[0]["version"] > versions:
-    isupdate = True
-    print(isupdate)
+if os.path.isfile('zhmarket.db'):
+    with open('zhmarket.db','r',encoding='utf8') as file:
+        alreadyprogramraw = file.read()
+        if alreadyprogramraw == '':
+            alreadyprogram = []
+        else:
+            alreadyprogram = eval(alreadyprogramraw)
+            for i in alreadyprogram:
+                AID = int(i['AID'])
+                if programlist[AID]['version'] > i['version']:
+                    readytoupdatelist.append(AID)
+                elif programlist[AID]['version'] == i['version']:
+                    readytoopenlist.append(AID)
+else:
+    file = open('zhmarket.db','w',encoding='utf8')
+    file.close()
 
 #运行图形程序
 app = wx.App()
--- py/Downloadbs.py
+++ py/Downloadbs.py
@@ -1,29 +1,62 @@
 import requests,warnings,sys,os
-with open('marketmain.cfg','r',encoding='utf8') as file:
-    programlistraw = file.read()
-    programlist = eval(programlistraw)
-filename = str(programlist[int(sys.argv[1])]["name"])
-if not os.path.exists('C:/Program Files/Download'):
-    os.makedirs('C:/Program Files/Download')
-path = 'C:/Program Files/Download'+filename
-if not os.path.exists(path):
-    os.makedirs(path) 
-           
-
 warnings.filterwarnings('ignore')
-def urldownload(url,filename):
+
+def urldownload(url,filename):#下载文件
     down_res = requests.get(url=url,verify=False)
     if not os.path.exists(filename):
         file = open(filename,'a')
         file.close()
     with open(filename,'wb') as file:
         file.write(down_res.content)
-urldownload('http://github.com/huangzherui/Download/raw/zbhedit/zip/7z.dll',(path+'/7z.dll'))
-urldownload('http://github.com/huangzherui/Download/raw/zbhedit/zip/7z.exe',(path+'/7z.exe'))
-for i in range(programlist[int(sys.argv[1])]["spilttotal"]):
-    s = '%03d' % int(i+1)
-    down_url = programlist[int(sys.argv[1])]["downloadurl"]
-    down_url = down_url.replace('$Appname',filename)
-    down_url = down_url.replace('$spiltnum',s)
-    urldownload(down_url,(path+filename+'.7z.'+s))
-os.system(('cd '+path+'&&copy /b '+filename+'.7z.00* '+filename+'.7z&&7z x '+filename+'.7z'))
+
+#打开marketmain.cfg
+with open('marketmain.cfg','r',encoding='utf8') as file:
+    programlistraw = file.read()
+    programlist = eval(programlistraw)
+
+#变量设置
+mode = sys.argv[1]
+AID = int(sys.argv[2])
+username = os.getenv('username')
+filename = str(programlist[AID]["name"])
+path = ('C:/Users/'+username+'/AppData/Local/ZHMarketDownload/')+filename+'/'
+
+if not mode == 'open':
+    #打开zhmarket.db
+    with open('zhmarket.db','r',encoding='utf8') as file:
+        alreadyprogramraw = file.read()
+        if alreadyprogramraw == '':
+            alreadyprogram = []
+        else:
+            alreadyprogram = eval(alreadyprogramraw)
+
+        if mode == 'update':
+            for i in range(len(alreadyprogram)):
+                if int(alreadyprogram[i]['AID']) == AID:
+                    alreadyprogram[i]['version'] = programlist[AID]['version']
+                    break
+        elif mode == 'download':
+            alreadyprogram.append({'AID':('%04d' % AID),'version':programlist[AID]['version']})
+    with open('zhmarket.db','w',encoding='utf8') as file:
+        file.write(str(alreadyprogram))
+
+    #检测文件夹
+    if not os.path.exists(('C:/Users/'+username+'/AppData/Local/ZHMarketDownload')):
+        os.makedirs(('C:/Users/'+username+'/AppData/Local/ZHMarketDownload'))
+    if not os.path.exists(path):
+        os.makedirs(path) 
+
+    urldownload('http://github.com/huangzherui/Download/raw/zbhedit/zip/7z.dll',(path+'7z.dll'))
+    urldownload('http://github.com/huangzherui/Download/raw/zbhedit/zip/7z.exe',(path+'7z.exe'))
+
+    for i in range(programlist[AID]["spilttotal"]):
+        s = '%03d' % int(i+1)
+        down_url = programlist[AID]["downloadurl"]
+        down_url = down_url.replace('$Appname',filename)
+        down_url = down_url.replace('$spiltnum',s)
+        urldownload(down_url,(path+filename+'.7z.'+s))
+    os.system(('powershell cd '+path+';./7z.exe x '+filename+'.7z.001'))
+    for i in range(programlist[AID]["spilttotal"]):
+        s = '%03d' % int(i+1)
+        os.system(('powershell del '+path+filename+'.7z.'+s))
+os.system(('powershell cd '+path+';start '+path+filename+'.exe'))
