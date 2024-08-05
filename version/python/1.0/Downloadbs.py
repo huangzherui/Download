ec0d3fa93658133152c8399a07a72b71e95e7948
huangzherui <166521162+huangzherui@users.noreply.github.com>
2024-08-05 20:28:40 +0800

    1.0

--- version/python/1.0/Downloadbs.py
+++ version/python/1.0/Downloadbs.py
@@ -19,7 +19,7 @@ mode = sys.argv[1]
 AID = int(sys.argv[2])
 username = os.getenv('username')
 filename = str(programlist[AID]["name"])
-path = ('C:/Users/'+username+'/AppData/Local/ZHMarketDownload/')+filename+'/'
+path = f'C:/Users/{username}/AppData/Local/ZHMarketDownload/{filename}/'
 
 if not mode == 'open':
     #打开zhmarket.db
@@ -41,22 +41,22 @@ if not mode == 'open':
         file.write(str(alreadyprogram))
 
     #检测文件夹
-    if not os.path.exists(('C:/Users/'+username+'/AppData/Local/ZHMarketDownload')):
-        os.makedirs(('C:/Users/'+username+'/AppData/Local/ZHMarketDownload'))
+    if not os.path.exists(f'C:/Users/{username}/AppData/Local/ZHMarketDownload'):
+        os.makedirs(f'C:/Users/{username}/AppData/Local/ZHMarketDownload')
     if not os.path.exists(path):
         os.makedirs(path) 
 
-    urldownload('http://github.com/huangzherui/Download/raw/zbhedit/zip/7z.dll',(path+'7z.dll'))
-    urldownload('http://github.com/huangzherui/Download/raw/zbhedit/zip/7z.exe',(path+'7z.exe'))
+    urldownload('http://github.com/huangzherui/Download/raw/zbhedit/zip/7z.dll',f'{path}7z.dll')
+    urldownload('http://github.com/huangzherui/Download/raw/zbhedit/zip/7z.exe',f'{path}7z.exe')
 
     for i in range(programlist[AID]["spilttotal"]):
         s = '%03d' % int(i+1)
         down_url = programlist[AID]["downloadurl"]
         down_url = down_url.replace('$Appname',filename)
         down_url = down_url.replace('$spiltnum',s)
-        urldownload(down_url,(path+filename+'.7z.'+s))
-    os.system(('powershell cd '+path+';./7z.exe x '+filename+'.7z.001'))
+        urldownload(down_url,f"{path}{filename}.7z.{s}")
+    os.system(f'powershell cd {path};./7z.exe x {filename}.7z.001')
     for i in range(programlist[AID]["spilttotal"]):
         s = '%03d' % int(i+1)
-        os.system(('powershell del '+path+filename+'.7z.'+s))
-os.system(('powershell cd '+path+';start '+path+filename+'.exe'))
+        os.system(f'powershell del {path}{filename}.7z.{s}')
+os.system(f'powershell cd {path};start {path}{filename}.exe')
